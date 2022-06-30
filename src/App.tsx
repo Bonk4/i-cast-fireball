651fba5c9dd4800e58bc14587df15a985ed20be2
dombrazeel <dominic.brazeel@fishtech.group>
2022-06-30 17:18:45 -0500

    init commit for v2 rewrite

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { NavBar } from './components/NavBar/navbar';
-import { InitiativeApp } from './components/InitiativeApp/InitiativeApp';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
+import InitiativeApp from './components/initiativeApp/initiativeApp';
 
 function App() {
   return (
     <div className="App">
-
-      <NavBar />
-
       <InitiativeApp />
-      
     </div>
   );
 }
