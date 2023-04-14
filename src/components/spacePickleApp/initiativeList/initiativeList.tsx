4c7f5930720fa61aa6375d231628ca5d2c4c34be
Dominic Brazeel <caboose271@gmail.com>
2023-04-13 21:17:16 -0500

    feat: adding compact mode to Space Pickle

--- src/components/spacePickleApp/initiativeList/initiativeList.tsx
+++ src/components/spacePickleApp/initiativeList/initiativeList.tsx
@@ -1,15 +1,17 @@
 import {
+  Badge,
   Button,
-  Container,
+  Center,
   Group,
   Paper,
+  SegmentedControl,
   SimpleGrid,
   Skeleton,
-  Space,
   Text,
   Title,
-} from "@mantine/core";
-import { Organism } from "../../../models/organism";
+} from '@mantine/core';
+import { Organism } from '../../../models/organism';
+import { useState } from 'react';
 
 export type InitiativeListProps = {
   organisms: Organism[];
@@ -20,6 +22,8 @@ const InitiativeList = ({
   organisms,
   updateOrganisms,
 }: InitiativeListProps) => {
+  const [size, updateSize] = useState('expanded');
+
   const removeOrganism = (index: number) => {
     let newOrganisms = organisms.slice();
     newOrganisms.splice(index, 1);
@@ -44,36 +48,66 @@ const InitiativeList = ({
           },
         })}
       >
-        {organisms.map((organism, i) => (
-          <>
-            <Paper
-              className={
-                organism.lightSide
-                  ? "init-light-organism"
-                  : "init-dark-organism"
-              }
-              shadow="md"
-              radius="md"
-              p="md"
-              withBorder
-            >
-              <Group position="apart">
-                <div></div>
-                <Title order={2}>{organism.lightSide ? "Light" : "Dark"}</Title>
-                <Button
-                  radius={"xl"}
-                  compact
-                  variant="subtle"
-                  color={"gray"}
-                  onClick={(e: any) => removeOrganism(i)}
-                >
-                  <i className="fa-solid fa-xmark"></i>
-                </Button>
-              </Group>
-              <Text>{`${organism.success} Success | ${organism.advantage} Advantage | ${organism.triumph} Triumph`}</Text>
-            </Paper>
-          </>
-        ))}
+        <SegmentedControl
+          value={size}
+          onChange={updateSize}
+          data={[
+            { label: 'Compact', value: 'compact' },
+            { label: 'Expanded', value: 'expanded' },
+          ]}
+        />
+        {organisms.map((organism, i) =>
+          size === 'compact' ? (
+            <>
+              <Center>
+                <div style={{ width: 150 }}>
+                  <Badge
+                    size="lg"
+                    className={
+                      organism.lightSide
+                        ? 'init-light-organism'
+                        : 'init-dark-organism'
+                    }
+                    fullWidth
+                  >
+                    {organism.lightSide ? 'Light' : 'Dark'}
+                  </Badge>
+                </div>
+              </Center>
+            </>
+          ) : (
+            <>
+              <Paper
+                className={
+                  organism.lightSide
+                    ? 'init-light-organism'
+                    : 'init-dark-organism'
+                }
+                shadow="md"
+                radius="md"
+                p="md"
+                withBorder
+              >
+                <Group position="apart">
+                  <div></div>
+                  <Title order={2}>
+                    {organism.lightSide ? 'Light' : 'Dark'}
+                  </Title>
+                  <Button
+                    radius={'xl'}
+                    compact
+                    variant="subtle"
+                    color={'gray'}
+                    onClick={(e: any) => removeOrganism(i)}
+                  >
+                    <i className="fa-solid fa-xmark"></i>
+                  </Button>
+                </Group>
+                <Text>{`${organism.success} Success | ${organism.advantage} Advantage | ${organism.triumph} Triumph`}</Text>
+              </Paper>
+            </>
+          ),
+        )}
       </SimpleGrid>
     </>
   );
